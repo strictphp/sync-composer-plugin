3f747097e3dc6bbd03ca159a40eb5738dde363bc
Martin Kluska <martin@kluska.cz>
2024-04-07 16:04:47 +0200

    Tweak the lowest version we can for green CI

--- src/UpdateScriptsCommand.php
+++ src/UpdateScriptsCommand.php
@@ -11,7 +11,7 @@ use Symfony\Component\Console\Output\OutputInterface;
 
 class UpdateScriptsCommand extends BaseCommand
 {
-    protected function configure()
+    protected function configure(): void
     {
         $this
             ->setName('update-scripts')
